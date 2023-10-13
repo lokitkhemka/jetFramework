6001bb58d83021593b9b6d46642c6f6a8abb0f37
lokitkhemka <loukitkhemka@gmail.com>
2023-10-13 12:25:16 +0530

    Added an abstract base class for 2D neighborhood search, which inherits from the Serialization base class.

--- src/jet/IO/Serialization/fbs_helpers.h
+++ src/jet/IO/Serialization/fbs_helpers.h
@@ -15,4 +15,82 @@ namespace jet
     {
         return fbs::Size2(vec.x, vec.y);
     }
+
+    inline fbs::Size3 JetToFbs(const Size3& vec)
+    {
+        return fbs::Size3(vec.x, vec.y, vec.z);
+    }
+
+    inline fbs::Vector2D JetToFbs(const Vector2D& vec)
+    {
+        return fbs::Vector2D(vec.x, vec.y);
+    }
+
+    inline fbs::Vector3D JetToFbs(const Vector3D& vec)
+    {
+        return fbs::Vector3D(vec.x, vec.y, vec.z);
+    }
+
+    inline Size2 FbsToJet(const fbs::Size2& vec)
+    {
+        return Size2({vec.x(), vec.y()});
+    }
+
+    inline Size3 FbsToJet(const fbs::Size3& vec)
+    {
+        return Size3({vec.x(), vec.y(), vec.z()});
+    }
+
+    inline Vector2D FbsToJet(const fbs::Vector2D& vec)
+    {
+        return Vector2D(vec.x(), vec.y());
+    }
+
+    inline Vector3D FbsToJet(const fbs::Vector3D& vec)
+    {
+        return Vector3D(vec.x(), vec.y(). vec.z());
+    }
+
+
+    template<typename GridType,
+            typename FbsFactoryFunc,
+            typename FbsGridType>
+    void SerializeGrid(flatbuffers::FlatBufferBuilder* builder,
+                        const std::vector<GridType>& GridList,
+                        FbsFactoryFunc func,
+                        std::vector<flatbuffers::Offset<FbsGridType>>* fbsGridList)
+    {
+        for(const auto& grid: GridList)
+        {
+            auto type = builder->CreateString(grid->typeName());
+            
+            std::vector<uint8_t> GridSerialized;
+            grid->Serialize(&GridSerialized);
+            auto fbsGrid = func(*builder, type, 
+                                    builder->CreateVector(GridSerialized.data(), GridSerialized.size()));
+            fbsGridList->push_back(fbsGrid);
+        }
+    }
+
+
+    template <typename FbsGridList, typename GridType, typename FactoryFunc>
+    void DeserializeGrid(FbsGridList* fbsGridList, FactoryFunc factoryFunc,
+                            std::vector<GridType>* GridList)
+    {
+        for (const auto& grid : (*fbsGridList))
+        {
+            auto type = grid->type()->c_str();
+
+            std::vector<uint8_t> GridSerialized(
+                grid->data()->begin(),
+                grid->data()->end()
+            );
+
+            auto NewGrid = factoryFunc(type);
+            NewGrid->Deserialize(GridSerialized);
+
+            GridList->push_back(NewGrid);
+        }
+    }
+    
 }
---
+++ src/jet/NeighborhoodSearch/point2_neighbor_searcher.cpp
@@ -0,0 +1,11 @@
+#include <jet.h>
+#include "point2_neighbor_searcher.h"
+
+namespace jet
+{
+    PointNeighborSearcher2::PointNeighborSearcher2()
+    {}
+
+    PointNeighborSearcher2::~PointNeighborSearcher2()
+    {}
+}
